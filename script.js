03751b3a73737d08e924c734764d5c205ee03e79
Devansh Chandra <155964011+Devansh149@users.noreply.github.com>
2025-07-30 21:13:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-var element = document.querySelectorAll(".element");
+var element=document.querySelectorAll(".element");
+element.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var img=e.querySelector("img");
+        img.style.opacity=1;
+    });
+    e.addEventListener("mouseleave",function(){
+        var img=e.querySelector("img");
+        img.style.opacity=0;
+    });
+    e.addEventListener("mousemove",function(dets){
+        var img=e.querySelector("img");
+        img.style.left=dets.clientX+"px";
+        img.style.top=dets.clientY+"px";
 
-element.forEach(function (val) {
-    val.addEventListener("mouseenter",function(){
-        val.childNodes[3].style.opacity="1";
-    })
-    val.addEventListener("mouseleave",function(){
-        val.childNodes[3].style.opacity="0";
-    })
-    val.addEventListener("mousemove",function(move){
-        val.childNodes[3].style.left=move.x+"px";
-        val.childNodes[3].style.top=move.y+"px";
-        
-    })
-})
+    });
+});
